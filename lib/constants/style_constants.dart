d5c84b0645f1f3233fe38a239cc5f5544cb059a6
Okyaaa <abi.mofra1@gmail.com>
2021-08-08 16:06:30 +0700

    last update

--- lib/constants/style_constants.dart
+++ lib/constants/style_constants.dart
@@ -6,3 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 var HTextStyle1 = GoogleFonts.nunito(fontWeight: FontWeight.w900, color: TextColor1, fontSize: 30);
 var HTextStyle2 = GoogleFonts.nunito(fontWeight: FontWeight.w500, color: TextColor1, fontSize: 24);
 var HTextStyle3 = GoogleFonts.nunito(fontWeight: FontWeight.w700, color: TextColor2, fontSize: 18);
+
+//loginpage text
+var LPTextStyle1 = GoogleFonts.nunito(fontWeight: FontWeight.w700, color: TextColor1, fontSize: 32);
+var LPTextStyle2 = GoogleFonts.nunito(fontWeight: FontWeight.w500, color: TextColor1, fontSize: 14);
--- lib/screens/homescreen/components/body.dart
+++ lib/screens/homescreen/components/body.dart
@@ -1,26 +1,49 @@
+import 'package:bukutamu_android/constants/style_constants.dart';
+import 'package:bukutamu_android/screens/homescreen/components/background.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Positioned(
-              top: 0,
-              right: -10,
-              child: Image.asset('assets/images/LPTopRight.png',
-              width: size.width * 0.3,
+        Image.asset('assets/images/loginpage.png'),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(top: 20)
+                ),
+                Text("Login",
+                  style: LPTextStyle1,)
+              ],
             )
           ),
-          Positioned(child: Image.asset('assets/images/LPBottomLeft.jpg'))
+          Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(top: 15)
+                  ),
+                  Text("Email",
+                    style: LPTextStyle2,)
+                ],
+              )
+          )
         ],
       ),
     );
   }
 }
+
+
