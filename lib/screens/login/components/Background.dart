845b3fbfbf4a90e0407659e42d02498c18a04e11
Okyaaa <abi.mofra1@gmail.com>
2021-08-11 14:30:03 +0700

    signup dan login fix

--- lib/screens/login/components/Background.dart
+++ lib/screens/login/components/Background.dart
@@ -11,26 +11,30 @@ class Background extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-              top: 0,
-              right: -30,
-              child: Image.asset('assets/images/loginpage/LPTopRight.png',
-                width: size.width * 0.3,
-              )
+      child: SingleChildScrollView(// Optional
+        child: Container(
+            height: size.height,
+            width: double.infinity,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              Positioned(
+                  top: 0,
+                  right: -30,
+                  child: Image.asset('assets/images/loginpage/LPTopRight.png',
+                    width: size.width * 0.3,
+                  )
+              ),
+              Positioned(
+                  bottom: 0,
+                  left: -20,
+                  child: Image.asset('assets/images/loginpage/LPBottomLeft.png',
+                  )
+              ),
+              child
+            ],
           ),
-          Positioned(
-              bottom: 0,
-              left: -20,
-              child: Image.asset('assets/images/loginpage/LPBottomLeft.png',
-              )
-          ),
-          child
-        ],
+        ),
       ),
     );
   }
--- lib/screens/signup/components/Background.dart
+++ lib/screens/signup/components/Background.dart
@@ -11,28 +11,32 @@ class Background extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-              top: -460,
-               right: 70,
-              child: Image.asset('assets/images/signuppage/SPTopLeft2.png',
-                height: 700,
-                width: 700,
-              )
-          ),
-          Positioned(
-              bottom: 0,
-              left: 200,
-              child: Image.asset('assets/images/signuppage/SPBottomRight.png',
-              )
-          ),
-          child
-        ],
-      ),
+        child: SingleChildScrollView(
+            child: Container(
+              height: size.height,
+              width: double.infinity,
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Positioned(
+                      top: -460,
+                      right: 70,
+                      child: Image.asset('assets/images/signuppage/SPTopLeft2.png',
+                        height: 700,
+                        width: 700,
+                      )
+                  ),
+                  Positioned(
+                      bottom: 0,
+                      left: 200,
+                      child: Image.asset('assets/images/signuppage/SPBottomRight.png',
+                      )
+                  ),
+                  child
+                ],
+              ),
+            )
+        )
     );
   }
 }
