70ac384c4fc2255e911963fe9876be6055e6e5ca
Okyaaa <abi.mofra1@gmail.com>
2021-09-18 16:57:23 +0700

    update tittle

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'BukuTamu',
+      title: 'Visity',
       home: WelcomePage(),
     );
   }
