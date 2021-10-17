2e0051800cbd0636d5d337cea08f685354d112d0
Okyaaa <abi.mofra1@gmail.com>
2021-10-17 17:49:13 +0700

    view in progress

--- lib/screens/home/components/Body.dart
+++ lib/screens/home/components/Body.dart
@@ -30,81 +30,36 @@ class _BodyState extends State<Body> {
         child: Container(
           padding: EdgeInsets.only(left: 16, right: 16),
           alignment: Alignment.center,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    height: 50.h,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                            child: SizedBox(
-                          child: Image.asset(
-                            'assets/icons/mainscreen/ProfileIcon_black.png',
-                          ),
-                        )),
-                        SizedBox(width: 20.w),
-                        Text(
-                          "Hello, Santoso!",
-                          style: mainSTextStyle1,
-                        ),
-                        SizedBox(
-                          width: 90.w,
-                        ),
-                      ],
-                    )),
-                SizedBox(height: 41.h),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Visitor",
-                        style: mainSTextStyle2,
-                      ),
-                      Text(
-                        "You have 5 visitors today",
-                        style: mainSTextStyle3,
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  child: FutureBuilder<Appointment>(
-                      future: _appointment,
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          return ListView.builder(
-                              itemCount: snapshot.data!.data.length,
-                              itemBuilder: (context, index) {
-                                var appointment = snapshot.data!.data[index];
-                                if (appointment.status == "waiting") {
-                                  return Container(
-                                    child: Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                          child: AppointmentCard(
-                                              size: size.width,
-                                              height: 196,
-                                              guestPurpose: appointment.purpose,
-                                              guestName:
-                                                  appointment.guest.name),
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                } else {
-                                  return SizedBox();
-                                }
-                              });
+          child: FutureBuilder<Appointment>(
+              future: _appointment,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                      itemCount: snapshot.data!.data.length,
+                      itemBuilder: (context, index) {
+                        var appointment = snapshot.data!.data[index];
+                        if (appointment.status == "waiting") {
+                          return Container(
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: AppointmentCard(
+                                      size: size.width,
+                                      height: 196,
+                                      guestPurpose: appointment.purpose,
+                                      guestName: appointment.guest.name),
+                                ),
+                              ],
+                            ),
+                          );
                         } else {
-                          return Center(child: CircularProgressIndicator());
+                          return SizedBox();
                         }
-                      }),
-                ),
-              ]),
+                      });
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              }),
         ),
       ),
     );
