b4b6c06f43cbb68824fea88a520244184ca5e892
rhezadr <dewanggarheza@gmail.com>
2021-12-02 21:12:15 +0700

    updated

--- lib/screens/login/LoginScreen.dart
+++ lib/screens/login/LoginScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:bukutamu_android/screens/login/components/Body.dart';
-import 'package:animate_do/animate_do.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
